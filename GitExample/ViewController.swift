1b739e19114ea8d7e705f622cbc5d5279e356870
Matt Mier <mmier@moranprinting.com>
2016-07-26 10:59:11 -0500

    doing this on my own now

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // extra comment!
+        // testing this out to see if I can do it on my own.
     }
 
     override func didReceiveMemoryWarning() {
